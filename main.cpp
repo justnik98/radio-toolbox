22298f0af29f13878c9e71d14c32852683fcb57a
Nikita Yankovskii <yannik98@yandex.ru>
2025-10-27 23:14:01 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
+#include <chrono>
 #include <iostream>
+#include "modulators/QamMod.h"
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto num = 1e6;
+    vector<bool> bits(num);
+    for (auto && bit : bits) {
+        bit = rand() % 2;
+    }
+
+    // for (auto && bit : bits) {
+    //     cout << bit;
+    // }
+    cout << "\n !!!! "<< endl;
+
+    QamMod qam_mod(16, 1, 1000, 1e-2);
+    auto start = chrono::high_resolution_clock::now();
+    auto signals = qam_mod.Mod(bits);
+    auto start2 = chrono::high_resolution_clock::now();
+    bits = qam_mod.Demod(signals);
+    auto stop = chrono::high_resolution_clock::now();
+
+    auto duration = duration_cast<chrono::microseconds>(start2 - start);
+    auto duration2 = duration_cast<chrono::microseconds>(stop - start2);
+
+    // for (auto && bit : bits) {
+    //     cout << bit;
+    // }
+    cout << endl << duration.count()<< endl << duration2.count() << endl;
+    cout << endl << "speed bits/sec: \n" << num*1e6/duration.count() << endl << num*1e6/duration2.count() << endl;
     return 0;
 }
